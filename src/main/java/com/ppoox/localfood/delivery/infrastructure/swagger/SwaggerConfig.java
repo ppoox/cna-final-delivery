122b9fe44972cdb55147a0a1b1b6ce96a92d2407
ppoox <ppoox@naver.com>
2024-04-04 13:08:22 +0900

    add openapi

---
+++ src/main/java/com/ppoox/localfood/delivery/infrastructure/swagger/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.ppoox.localfood.delivery.infrastructure.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    public OpenAPI baseApi() {
+        return new OpenAPI().info(new Info().title("Delivery Service")
+                .description("Delivery Service API"));
+    }
+
+    @Bean
+    public GroupedOpenApi allApi() {
+        return GroupedOpenApi.builder().group("All")
+                .packagesToScan("com.ppoox.localfood.delivery.adapter.in.presentation").build();
+    }
+
+}
