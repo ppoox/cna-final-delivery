812a6d7f16647bd4c281286b411f4a4e0ecdc553
ppoox <ppoox@naver.com>
2024-04-04 10:37:18 +0900

    add dependency and bean error

--- src/main/java/com/ppoox/localfood/delivery/adapter/in/presentation/DeliveryController.java
+++ src/main/java/com/ppoox/localfood/delivery/adapter/in/presentation/DeliveryController.java
@@ -2,11 +2,10 @@ package com.ppoox.localfood.delivery.adapter.in.presentation;
 
 import com.ppoox.localfood.delivery.application.port.in.presentation.DeliveryPresentationPort;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/delivery")
+//@RequestMapping("/api/v1/delivery")
 @RequiredArgsConstructor
 public class DeliveryController {
 
--- src/main/java/com/ppoox/localfood/delivery/adapter/out/persistence/DeliveryPersistenceAdapter.java
+++ src/main/java/com/ppoox/localfood/delivery/adapter/out/persistence/DeliveryPersistenceAdapter.java
@@ -1,12 +1,12 @@
 package com.ppoox.localfood.delivery.adapter.out.persistence;
 
-import com.ppoox.localfood.delivery.application.port.in.presentation.DeliveryPresentationPort;
+import com.ppoox.localfood.delivery.application.port.out.persistence.DeliveryPersistencePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class DeliveryPersistenceAdapter implements DeliveryPresentationPort {
+public class DeliveryPersistenceAdapter implements DeliveryPersistencePort {
 
     private final DeliveryRepository deliveryRepository;
 
